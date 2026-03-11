69d7e9539050f1ff02c39df1b964a89224ccefb8
André Selva <selvaandre99@gmail.com>
2026-03-11 20:02:42 -0300

    ajusta appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './modules/events/events.module';
+import { DatabaseModule } from './modules/database/database.module';
+import { GroupsModule } from './modules/groups/groups.module';
+import { UserModule } from './modules/user/user.module';
+import { VolunteersModule } from './modules/volunteers/volunteers.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, EventsModule, GroupsModule, UserModule, VolunteersModule],
   controllers: [AppController],
   providers: [AppService],
 })
