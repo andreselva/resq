20457a87fbe8f8dc189ce0b8cf7fa6b1d2be6178
André Selva <selvaandre99@gmail.com>
2026-03-25 19:21:15 -0300

    ajusta spec dos services

--- src/modules/events/events.service.spec.ts
+++ src/modules/events/events.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventsService } from './events.service';
+import { EventsRepository } from './events.repository';
+import { EventsProcessorService } from './events-processor.service';
 
 describe('EventsService', () => {
   let service: EventsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventsService],
+      providers: [
+        EventsService,
+        {
+          provide: EventsRepository,
+          useValue: {},
+        },
+        {
+          provide: EventsProcessorService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<EventsService>(EventsService);
