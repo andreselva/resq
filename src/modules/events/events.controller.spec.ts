fde95051479f441a3234b3080d7357442b8dc8c5
André Selva <selvaandre99@gmail.com>
2026-03-25 19:18:09 -0300

    ajustes nos specs

--- src/modules/events/events.controller.spec.ts
+++ src/modules/events/events.controller.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { EventsController } from './events.controller';
+import { EventsService } from './events.service';
+import { EventsRepository } from './events.repository';
+import { EventsProcessorService } from './events-processor.service';
 
-describe('EventsController', () => {
-  let controller: EventsController;
+describe('EventsService', () => {
+  let service: EventsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [EventsController],
+      providers: [
+        EventsService,
+        {
+          provide: EventsRepository,
+          useValue: {},
+        },
+        {
+          provide: EventsProcessorService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
-    controller = module.get<EventsController>(EventsController);
+    service = module.get<EventsService>(EventsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
