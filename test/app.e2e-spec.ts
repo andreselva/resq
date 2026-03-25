61985e79a4b4cc91d65188ab9fd29257c3d267dd
André Selva <selvaandre99@gmail.com>
2026-03-25 19:28:20 -0300

    ajustes teste final

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Health (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('/health (GET)', async () => {
+    const server = app.getHttpServer() as unknown as Parameters<
+      typeof request
+    >[0];
+
+    const response = await request(server).get('/health');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ status: 'ok' });
+  });
+});
